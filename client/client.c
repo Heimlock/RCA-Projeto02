6d782fb74e933b1a9c88f28b735ca4230557a54f
brunobannwart <bbannwart@yahoo.com.br>
2019-06-08 11:13:48 -0300

    Inicio da logica (conexão com servidor e com outro usuario)

--- client/client.c
+++ client/client.c
@@ -20,74 +20,21 @@ int main(int argc, char const *argv[]) {
     fprintf(stdout, "[%d] | Client Module Initialized!\n", getpid());
     fflush(stdout);
 
-    struct SPDT_Command *command;
-    struct User_t *user;
-
-   if(argc != 3) {
+   if(argc != 2) {
         fprintf(stderr, "[%d] | Error! Not a Valid Input!\n", getpid());
-        fprintf(stderr, "[%d] | Usage: ./client <ServerIp> <Port>\n", getpid());
+        fprintf(stderr, "[%d] | Usage: ./client <ClientID> \n", getpid());
         fflush(stderr);
         exit(-1);
     }
 
-    if(commOps.initClient(&local, 0) < 0) {
+    if((commOps.initClient(&local, 0)) < 0) {
         fprintf(stderr, "[%d] | Error! Init Socket Client!\n", getpid());
         fflush(stderr);
         exit(-2);
     }
 
-    if(commOps.connect(&local, &remote, argv[1], atoi(argv[2])) < 0) {
-        fprintf(stderr, "[%d] | Error! Connect to Server Socket!\n", getpid());
-        fflush(stderr);
-        exit(-2);
-    }
-
-    fprintf(stdout, "[%d] | logIn.\n", getpid());
-    fflush(stdout);
-    char *value = (char *)malloc(sizeof(char));
-    value[0] = 0xAA;
-    value[1] = 0xBB;
-    value[2] = 0xCC;
-    value[3] = 0xDD;
-
-    command = newCommand(LogIn, 4 * sizeof(char), value);
-
-    printCommand(getpid(), *command);
-
-    if(sendCommand(&local, (*command)) < 0) {
-        fprintf(stderr, "[%d] | Error! Failed to send.\n", getpid());
-        fflush(stderr);
-        exit(-4);
-    }
-
-    sleep(1000);
-
-    fprintf(stdout, "[%d] | requestClient.\n", getpid());
-    fflush(stdout);
-    command = newCommand(RequestClient, 4 * sizeof(char), value);
-
-    if(sendCommand(&local, (*command)) < 0) {
-        fprintf(stderr, "[%d] | Error! Failed to send.\n", getpid());
-        fflush(stderr);
-        exit(-5);
-    } 
-
-    user  = (User_t *) receiveStruct(&local, RequestClient);
-    fprintf("Telefone: %s\n", user->id);
-
-    sleep(1000);
-
-    fprintf(stdout, "[%d] | logOut.\n", getpid());
-    fflush(stdout);
-    command = newCommand(LogOut, 4 * sizeof(char), value);
-
-    if(sendCommand(&local, (*command)) < 0){
-        fprintf(stderr, "[%d] | Error! Failed to send.\n", getpid());
-        fflush(stderr);
-        exit(-6);
-    }
-
+    initSharedData();
+    connectToServer(argv[1]);
     commOps.close(&local);
-    // commOps.close(&remote);
     return 0;
 }
--- client/clientFunctions.c
+++ client/clientFunctions.c
@@ -9,3 +9,129 @@
  *
  *	 Desenvolvimento dos Recursos Referentes ao Cliente
  */
+
+ #include "./client.h"
+
+ void connectToServer(char *id){
+    char ip[9] = "localhost";
+    int port = 5000, connection = 1, command_type;
+    User_t *user;
+
+    while(connection){
+        if((commOps.connect(&local, &remote, ip, port)) < 0){
+            fprintf(stderr, "[%.4d] | Error! Client couldn't connect to server.\n", getpid());
+            fflush(stderr);
+            perror("connectToServer");
+            exit(-3);
+        }
+
+        scanf("%d", &command_type);
+
+        switch(command_type){
+            case LogIn:     logIn(id);
+                            commOps.close(&remote);
+                            break;
+
+            case LogOut:    logOut(id);
+                            commOps.close(&remote);
+                            connection = 0;
+                            break;
+            
+            case RequestClient: user = requestClient();
+                                commOps.close(&remote);
+                                //connectToClient(user);
+                                break;
+
+            default:    fprintf(stdout, "[%.4d] | Command invalid.\n", getpid());
+                        fflush(stdout);
+                        break;
+        }
+
+        if(connection == 1){
+            fprintf(stdout, "[%.4d] | Connect with server again?\n");
+            scanf("%d", &connection);
+        }
+    }
+ }
+
+ void logIn(char *id){
+    fprintf(stdout, "[%.4d] | Log In.\n", getpid());
+    fflush(stdout);
+    struct SPDT_Command *command;
+
+    command = newCommand(LogIn, 9*sizeof(char), id);
+
+    if((sendCommand(&local, (*command))) < 0) {
+        fprintf(stderr, "[%d] | Error! Failed to send.\n", getpid());
+        fflush(stderr);
+        perror(logIn);
+        exit(-4);
+    }
+ }
+
+ void logOut(char *id){
+    fprintf(stdout, "[%.4d] | Log Out.\n", getpid());
+    fflush(stdout);
+    struct SPDT_Command *command;
+
+    command = newCommand(LogOut, 9*sizeof(char), id);
+
+    if((sendCommand(&local, (*command))) < 0) {
+        fprintf(stderr, "[%d] | Error! Failed to send.\n", getpid());
+        fflush(stderr);
+        perror(logOut);
+        exit(-5);
+    }     
+ }
+
+ User_t* requestClient(){
+    fprintf(stdout, "[%.4d] | Request Client.\n", getpid());
+    fflush(stdout);
+    struct SPDT_Command *command;
+    struct User_t *user;
+    char cellphone[9];
+
+    fprintf(stdout, "[%.4d] | User cellphone?\n", getpid());
+    scanf("%s", cellphone);
+
+    command = newCommand(RequestClient, 9*sizeof(char), cellphone);
+
+    if((sendCommand(&local, (*command))) < 0){
+        fprintf(stderr, "[%d] | Error! Failed to send.\n", getpid());
+        fflush(stderr);
+        perror(requestClient);
+        exit(-6);
+    }
+
+    user = receiveStruct(&local, RequestClient);
+
+    if(user == NULL){
+        fprintf(stderr, "[%d] | Error! Failed to receive user.\n", getpid());
+        fflush(stderr);
+        perror(requestClient);
+        exit(-7);
+    }
+
+    return user;
+ }
+
+ /*void connectToClient(struct User_t *user){
+    if((commOps.connect(&local, &remote, user->addr.sin_addr, user->addr.sin_ports)) < 0){
+        fprintf(stderr, "[%.4d] | Error! Client couldn't connect to user.\n", getpid());
+        fflush(stderr);
+        perror("connectToClient");
+        exit(-8);
+    }
+ }*/
+
+ void  initSharedData() {
+    mutex_remote = mutexInit();
+    mutex_list_messages = mutexInit();
+
+    if(mutex_remote == NULL || mutex_list_messages == NULL){
+        mutexDestroy(mutex_remote);
+        mutexDestroy(mutex_list_messages);
+        commOps.close(&local);
+        exit(-2);
+    }
+}
